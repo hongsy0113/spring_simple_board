7da6bf62a1941e4142d10660a1f4bf9102b0d036
SungYoon Hong <hsy0113@korea.ac.kr>
2022-04-14 13:08:52 +0900

    [FEAT] alert message

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -23,11 +23,14 @@ public class BoardController {
     }
 
     @PostMapping("/board/writepro")
-    public String boardWritePro(Board board){
+    public String boardWritePro(Board board, Model model){
 
         boardService.write(board);
 
-        return "";
+        model.addAttribute("message", "글 작성이 완료되었습니다.");
+        model.addAttribute("searchUrl", "/board/list");
+
+        return "message";
     }
 
     @GetMapping("/board/list")
@@ -62,13 +65,17 @@ public class BoardController {
     }
 
     @PostMapping("/board/update/{id}")
-    public String boardUpdate(@PathVariable("id") Integer id, Board board){
+    public String boardUpdate(@PathVariable("id") Integer id, Board board, Model model){
 
         Board boardTemp = boardService.boardView(id);
         boardTemp.setTitle(board.getTitle());
         boardTemp.setContent(board.getContent());
 
         boardService.write(boardTemp);
-        return "redirect:/board/list";
+        //return "redirect:/board/list";
+
+        model.addAttribute("message", "글 수정이 완료되었습니다");
+        model.addAttribute("searchUrl", "/board/list");
+        return "message";
     }
 }
