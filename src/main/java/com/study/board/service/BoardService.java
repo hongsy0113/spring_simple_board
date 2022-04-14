ff86e7729085f000e552bfc13247c02c779c39ac
SungYoon Hong <hsy0113@korea.ac.kr>
2022-04-14 13:41:15 +0900

    [FEAT] file upload

--- src/main/java/com/study/board/service/BoardService.java
+++ src/main/java/com/study/board/service/BoardService.java
@@ -4,8 +4,11 @@ import com.study.board.entity.Board;
 import com.study.board.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class BoardService {
@@ -14,7 +17,27 @@ public class BoardService {
     private BoardRepository boardRepository;
 
     // 글 작성 처리
-    public void write(Board board) {
+    public void write(Board board, MultipartFile file) throws Exception{
+
+        // 프로젝트 경로 + 파일 저장 경로를 담아주게 됨
+       String projectPath = System.getProperty("user.dir") + "\\src\\main\\resources\\static\\files";
+
+       //식별자 생성
+        UUID uuid = UUID.randomUUID();
+
+
+        // 원래 파일이름에 랜덤 식별자를 붙여서 저장
+        String fileName = uuid + "_" + file.getOriginalFilename();
+
+       // 경로와 이름을 파라미터로 넣는다
+       File saveFile = new File(projectPath, fileName);
+
+       file.transferTo(saveFile);
+
+       // 실제 db 필드에 저장하기 위해서 set 하는 과정 필요함
+        board.setFilename(fileName);
+        // 서버에서 접근할 때는 static 아래의 경로로만 접근 가능하다
+        board.setFilepath("/files/" + fileName);
 
         boardRepository.save(board);
     }
